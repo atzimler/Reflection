9689b418d5979f445dab8a8d31aa98a0a61fb32b
Attila Tamas Zimler <hijaszu@icloud.com>
2016-10-25 23:31:08 +1100

    Code cleanup.

--- ATZ.Reflection.Tests/Template.cs
+++ ATZ.Reflection.Tests/Template.cs
@@ -1,5 +1,6 @@
 ﻿namespace ATZ.Reflection.Tests
 {
+    // ReSharper disable once UnusedTypeParameter => T parameter is required to be able to test the generic with type parameter.
     public class Template<T>
     {
     }
--- ATZ.Reflection/TypeExtensions.cs
+++ ATZ.Reflection/TypeExtensions.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ATZ.Reflection
 {
