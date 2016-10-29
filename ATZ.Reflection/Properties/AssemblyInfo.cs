cff2be6bf59f42be7ac1f0aed1658d5f7d8b24b6
Attila Tamas Zimler <hijaszu@icloud.com>
2016-10-29 17:50:31 +1100

    Changed version number structure to be able to follow semantic versioning.

--- ATZ.Reflection/Properties/AssemblyInfo.cs
+++ ATZ.Reflection/Properties/AssemblyInfo.cs
@@ -31,4 +31,13 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+
+// Semantic versioning - from http://semver.org
+// Summary
+//Given a version number MAJOR.MINOR.PATCH, increment the:
+//MAJOR version when you make incompatible API changes,
+//MINOR version when you add functionality in a backwards-compatible manner, and
+//PATCH version when you make backwards-compatible bug fixes.
+//Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+[assembly: AssemblyVersion("1.1.0.*")]
+
