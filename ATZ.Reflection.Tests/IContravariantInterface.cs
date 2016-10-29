3fe9d6bb9ed6077e3af034ac8ce38a028e8e979b
Attila Tamas Zimler <hijaszu@icloud.com>
2016-10-29 17:49:57 +1100

    Code cleanup.

--- ATZ.Reflection.Tests/IContravariantInterface.cs
+++ ATZ.Reflection.Tests/IContravariantInterface.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ATZ.Reflection.Tests
+﻿namespace ATZ.Reflection.Tests
 {
     public interface IContravariantInterface<in T>
     {
