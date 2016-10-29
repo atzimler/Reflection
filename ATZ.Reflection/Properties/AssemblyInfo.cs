574cfa9b82acab6dd1ec7d27a6c4335859168114
Attila Tamas Zimler <hijaszu@icloud.com>
2016-10-29 18:02:37 +1100

    Avoid mentioning the word bug in a comment, because ReSharper picks it up into the TODO explorer.

--- ATZ.Reflection/Properties/AssemblyInfo.cs
+++ ATZ.Reflection/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 //Given a version number MAJOR.MINOR.PATCH, increment the:
 //MAJOR version when you make incompatible API changes,
 //MINOR version when you add functionality in a backwards-compatible manner, and
-//PATCH version when you make backwards-compatible bug fixes.
+//PATCH version when you make backwards-compatible fixes.
 //Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
 [assembly: AssemblyVersion("1.1.0.*")]
 
