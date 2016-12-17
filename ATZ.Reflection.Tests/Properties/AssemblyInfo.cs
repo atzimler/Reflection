6d601284b54374f1552d62e8e4eed61c044c9ef9
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2016-12-18 08:20:30 +1100

    Extracting AssemblyVersion into AssemblyVersion.cs, so that it can be edited in one session for all of the projects. Part of building a better build system, as now Jenkins will be easily able to change the last part to the build number. Needed for having the same 4th component across multiple target frameworks.

--- ATZ.Reflection.Tests/Properties/AssemblyInfo.cs
+++ ATZ.Reflection.Tests/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d45f6422-60f0-4327-8d02-d57f0e3d864c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.*")]
